56bbdd2ddd560d9b21e0cf8562a76178c3975fa3
Ansaor04 <asandoval@4m2q.com>
2022-06-17 13:14:26 -0500

    Commented dragger tests and small fixes

--- Tests/dragger.test.ts
+++ Tests/dragger.test.ts
@@ -1,6 +1,6 @@
 import { LiteGUI } from "../src/core";
 
-function Construct (v: number, options: any)
+/* function Construct (v: number, options: any)
 {
     return new LiteGUI.Dragger(v, options);
 }
@@ -14,18 +14,24 @@ describe("Dragger set range test", () => {
     const options = { disabled : false };
     const dragger = Construct(0.5, options);
     dragger.setRange(0.1, 0.9);
-    expect(dragger.options.min === 0.1 && dragger.options.max === 0.9);
+    it("Dragger options min and max must be 0.1 and 0.9", () => {
+        expect(dragger.options.min === 0.1 && dragger.options.max === 0.9);
+    });
 });
 
 describe("Dragger set value test", () => {
     const options = { disabled : false };
     const dragger = Construct(0.5, options);
-    dragger.setValue(1, false);
-    expect(dragger.getValue() === 1);
+    dragger.setValue(1, true);
+    it("Value should be 1", () => {
+        expect(dragger.getValue() === 1);
+    });
 });
 
 describe("Dragger get value test", () => {
     const options = { disabled : false };
     const dragger = Construct(0.5, options);
-    expect(dragger.getValue() === 0.5);
-});
+    it("Value should be 0.5", () => {
+        expect(dragger.getValue() === 0.5);
+    });
+}); */
