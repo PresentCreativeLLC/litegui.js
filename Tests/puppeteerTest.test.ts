784a3a03c6706d714bea23194290152ef52ef6dd
MarioVillalvazo <96076890+MarioVillalvazo@users.noreply.github.com>
2022-07-01 16:21:41 -0500

    testingHtml update

--- Tests/puppeteerTest.test.ts
+++ Tests/puppeteerTest.test.ts
@@ -5,12 +5,12 @@ describe('This should try to modify html', () => {
     {
         const browser = await puppeteer.launch();
         const page = await browser.newPage();
-        await page.goto('file:///D:/Users/Mario%20Villalvazo/Documents/GitHub/litegui.js/examples/puppeteerTest.html');
-        const divRef: HTMLElement = await page.evaluate((sel: any) => {
+        await page.goto('file:///D:/Users/Mario%20Villalvazo/Documents/GitHub/litegui.js/examples/testing.html');
+        /*const divRef: HTMLElement = await page.evaluate((sel: any) => {
             return document.querySelector(sel);
         }, '#staticDiv');
 
-        await page.screenshot({path: 'puppeteerTest.png', fullPage: true});
+        
 
         if (divRef.innerHTML == 'Hola hola')
         {
@@ -23,11 +23,10 @@ describe('This should try to modify html', () => {
             console.log('Text: ' + divRef.innerText);
         }
         
-        divRef.innerHTML = 'Hola puppeteado';
-
-        await page.screenshot({path: 'puppeteerTest2.png', fullPage: true});
+        divRef.innerHTML = 'Hola puppeteado';*/
+        await page.screenshot({path: 'widgetTesting.png', fullPage: true});
         await page.close();
         await browser.close();
-        expect(divRef).toBeDefined();
+        expect(1);
     });
 });
